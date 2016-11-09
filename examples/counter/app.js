ba40b6b0e92763c00398848034b26f68dfa8798e
uojo <499620917@qq.com>
2016-11-09 10:52:16 +0800

    save

--- examples/counter/app.js
+++ examples/counter/app.js
@@ -5,20 +5,20 @@ import Counter from './Counter.vue'
 import store from './store'
 
 new Vue({
-  el: '#app',
-  data:{
-	a1:1,
-  },
-  state:{
-	s1:1,
-  },
-  store,
-  render: S => {
-	  // console.log("Self", S);
-	  // console.log("组件", Counter);
-	  // 相当与将组件 Counter.vue 附着到 Vue的options上，以便将 template 和 components ... 加上
-	  return S(Counter);
-  }
+	el: '#app',
+	data:{
+		a1:1,
+	},
+	state:{
+		s1:1,
+	},
+	store,
+		render: S => {
+		// console.log("Self", S);
+		// console.log("组件", Counter);
+		// 相当与将组件 Counter.vue 附着到 Vue的options上，以便将 template 和 components ... 加上
+		return S(Counter);
+	}
 })
 
 
